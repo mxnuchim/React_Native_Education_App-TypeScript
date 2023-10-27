2e74f46b18bd18a638fda4f5efb741e9b1290cdf
mxnuchim <manuchimoliver779@gmail.com>
2023-10-27 13:50:40 +0100

    feat: provinces screen

--- screens/auth/SelectProvinceScreen.tsx
+++ screens/auth/SelectProvinceScreen.tsx
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParamList } from '../../types';
+import HeaderText from '../../components/headerText';
+import ProvinceSelector from '../../components/provinceSelector';
+import Button from '../../components/button';
 
 const SelectProvinceScreen = () => {
+  const navigation = useNavigation<RootStackParamList>();
+
   return (
-    <View>
-      <Text>SelectProvinceScreen</Text>
-    </View>
+    <SafeAreaView className="px-7 py-8">
+      {/** ============== Header text component =========== */}
+      <HeaderText text={"What's your province?"} />
+
+      {/** ============== Select Grades ===================== */}
+      <View className="mt-10">
+        <ProvinceSelector />
+      </View>
+
+      {/** ========= Action button ================== */}
+      <View className="mt-[25%]">
+        <Button
+          primaryBtnText={'Next'}
+          onPrimaryBtnPress={() => navigation.navigate('Home')}
+          secondaryBtnText2={'Skip'}
+          onSecondaryBtnPress={() => navigation.navigate('Home')}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
